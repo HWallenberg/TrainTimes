06d1330bead13acf44120fcafc4c5db679d7ca9d
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2023-05-02 14:57:54 +0100

    Added New test and a test that will fail to test CI/CD in github

--- TrainTimesTests/LogicTests.cs
+++ TrainTimesTests/LogicTests.cs
@@ -21,6 +21,7 @@ namespace TrainTimesTests
             Assert.AreEqual("940GZZLUPCC", arrivals["Northbound - Platform 1"][0].naptanId);
         }
 
+        [TestMethod]
         public async Task GetAllOxfordStreetArrivals()
         {
             Dictionary<string, List<StationArrival>> arrivals = new Dictionary<string, List<StationArrival>>();
@@ -29,6 +30,17 @@ namespace TrainTimesTests
             Assert.IsTrue(arrivals.Count > 0);
             Assert.IsTrue(arrivals.ContainsKey("Southbound - Platform 3"));
             Assert.AreEqual("940GZZLUOXC", arrivals["Southbound - Platform 3"][0].naptanId);
+        }
+
+        [TestMethod]
+        public async Task GetAllOxfordStreetArrivals_FAILTEST()
+        {
+            Dictionary<string, List<StationArrival>> arrivals = new Dictionary<string, List<StationArrival>>();
+            StationArrivals nt = new StationArrivals();
+            arrivals = await nt.GetPlatformsArrivals("oxford");
+            Assert.IsTrue(arrivals.Count > 0);
+            Assert.IsTrue(arrivals.ContainsKey("Southbound - Platform 3"));
+            Assert.AreEqual("940GZZLUPCC", arrivals["Southbound - Platform 3"][0].naptanId);
         }
 
     }
