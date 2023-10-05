84c0d8fe92deab49ba3f2d8f2c673499bb4afc2c
hadleigh <hadleigh.wallenberg@sky.com>
2023-10-05 11:40:00 +0100

    more changes for support file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  supportFile: "e2e.js",
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
